a2aa56b132d0535b1d844f77821ebb12f63bed69
Peter Sung <petersung21@gmail.com>
2015-03-17 19:58:58 -0400

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 
-app.set('port', (process.env.PORT || 5000));
+/*app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
@@ -12,16 +13,27 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-});
+});*/
 
 MongoClient.connect('mongodb://phsung:Raptors12@ds047571.mongolab.com:47571/db_ticketing', function (err, db) {
     if(err) throw err;
 
-    var collection = db.collection('testData');
-    collection.insert({a:2}, function(err, docs) {
-        collection.count(function(err, count) {
-            console.log(format("count = %s", count));
+    var collection = db.collection('test_insert');
+    collection.insert({b:2}, function(err, docs) {
+        collection.find().toArray(function(err, results) {
+            console.log (format("count = %s"),results.length)
+            console.dir(results + "YOOO");
+            // Let's close the db
             db.close();
-        });
+            
+        collection.query(function(error,data){
+                          if (error) { throw error; return; }
+            console.log (data);
+            
+        });             
     });
+    
+    });
+
+    // Locate all the entries using find
 });
