9adc659b67e766f5d3f946721be23ce713e64c8e
Peter Sung <petersung21@gmail.com>
2015-03-19 22:08:07 -0400

    making it even better2

--- index.js
+++ index.js
@@ -5,41 +5,57 @@ var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-/*app.use(bodyParser.json());
-app.use()
-
-mongoose.connect('mongodb://phsung:Raptors12@ds047571.mongolab.com:47571/db_ticketing',function(err){
-    if (err){
-        console.log ("Connection Failed");
-    } else{
-        console.log ("Connection Successful");
-    }
-});222*/
-
-//MongoClient.connect('mongodb://phsung:Raptors12@ds047571.mongolab.com:47571/db_ticketing', function (err, db) {
-//    if(err) throw err;
+//app.set('port', (process.env.PORT || 5000));
+//app.use(express.static(__dirname + '/public'));
 //
-//    var collection = db.collection('test_insert');
-//    collection.insert({b:2}, function(err, docs) {
-//        collection.find().toArray(function(err, results) {
-//            console.log (format("count = %s"),results.length)
-//            console.dir(results + "YOOO");
-//            // Let's close the db
-//            db.close();
-//    });
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+//
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'));
+//});
+
+//app.use(bodyParser.json());
+//app.use(bodyParse.urlencoded({
+//    extended:true;
+//}));
+//
+//app.use (function(req,res,next){
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"))
+//    next();
+//});
+//
+//app.get('/getJSON', function (req,res,next){
 //    
-//    });
+//});
+//
+//app.post('/receiveJSON', function(req,res,next){
+//    
+//});
+//
+//mongoose.connect('mongodb://phsung:Raptors12@ds047571.mongolab.com:47571/db_ticketing',function(err){
+//    if (err){
+//        console.log ("Connection Failed");
+//    } else{
+//        console.log ("Connection Successful");
+//    }
+//});222
+
+MongoClient.connect('mongodb://phsung:Raptors12@ds047571.mongolab.com:47571/db_ticketing', function (err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({b:2}, function(err, docs) {
+        collection.find().toArray(function(err, results) {
+            console.log (format("count = %s"),results.length)
+            console.dir(results + "YOOO");
+            // Let's close the db
+            db.close();
+    });
+    
+    });
 
     // Locate all the entries using find
 //});
