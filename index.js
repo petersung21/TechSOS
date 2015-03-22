9acc9e14d7dacd0528bde35bf83a45cc9aa27bdc
Peter Sung <petersung21@gmail.com>
2015-03-22 00:56:30 -0400

    app commit

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
  
-app.use (function(req,res,next){
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+app.all (function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
  
